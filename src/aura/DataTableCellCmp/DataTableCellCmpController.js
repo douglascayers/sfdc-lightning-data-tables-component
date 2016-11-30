19ae7781d9455869235a0b83536b05d7f18fe2af
Doug Ayers <douglascayers@gmail.com>
2016-11-29 20:43:45 -0600

    comments

--- src/aura/DataTableCellCmp/DataTableCellCmpController.js
+++ src/aura/DataTableCellCmp/DataTableCellCmpController.js
@@ -4,6 +4,14 @@
         var row = component.get( 'v.row' );
         var column = component.get( 'v.column' );
 
+        // the column's name might be a single property on the object
+        // like 'Subject' or it might be a compound reference
+        // like 'Who.Name' so we split the string into its parts
+        // and try to traverse the object graph through the properties.
+        //
+        // if the row does not have the full property graph
+        // then null is returned, otherwise the value at the end of the rainbow.
+
         var fields = column.get( 'v.name' ).split( '.' );
 
         var value = null;
