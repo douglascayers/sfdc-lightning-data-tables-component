4fddd29a444704e71f1dd44fa0c789fa3ca3f945
Doug Ayers <douglascayers@gmail.com>
2016-12-22 11:02:42 -0600

    handle unrender to clear interval

--- src/aura/DataTableCmp/DataTableCmpRenderer.js
+++ src/aura/DataTableCmp/DataTableCmpRenderer.js
@@ -18,9 +18,11 @@
 
         // periodically attach the scroll event listener
         // so that we aren't taking action for all events
-        setInterval( $A.getCallback( function() {
+        var scrollCheckIntervalId = setInterval( $A.getCallback( function() {
 
-            if ( didScroll ) {
+            // since this function is called asynchronously outside the component's lifecycle
+            // we need to check if the component still exists before trying to do anything else
+            if ( didScroll && component.isValid() ) {
 
                 didScroll = false;
 
@@ -33,6 +35,20 @@
             }
 
         }), 1000 );
+
+        component.set( 'v.scrollCheckIntervalId', scrollCheckIntervalId );
+
+    },
+
+    unrender : function( component, helper ) {
+
+        this.superUnrender();
+
+        var scrollCheckIntervalId = component.get( 'v.scrollCheckIntervalId' );
+
+        if ( !$A.util.isUndefinedOrNull( scrollCheckIntervalId ) ) {
+            window.clearInterval( scrollCheckIntervalId );
+        }
 
     }
 })
